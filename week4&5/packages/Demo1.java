d68a4bfbdfbfcfe4c73971c57fd710c4d626626e
akshithaamirishetty <amirishettyakshitha@gmail.com>
2026-03-04 12:15:41 +0530

    Add files via upload

---
+++ week4&5/packages/Demo1.java
@@ -0,0 +1,12 @@
+import p1.Protection;
+import p1.Derived;
+import p1.SamePackage;
+public class Demo1
+{
+	public static void main(String[] args) 
+	{
+		Protection ob1=new Protection();
+		Derived ob2 = new Derived();
+		SamePackage ob3=new SamePackage();
+	}
+}
